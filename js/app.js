cb61d53dbd8dd31de264590e4edc1d7944e55f49
Maxim Kholopov <decimviralis@gmail.com>
2017-11-26 22:32:36 +0700

    fix-zdarova

--- js/app.js
+++ js/app.js
@@ -1,12 +1,27 @@
+let blogPost = [
+    {
+        subject: 'Саша Печкин',
+        text: 'В четверг, четвертого числа...'
+    },
+    {
+        subject: 'Просто Вася',
+        text: 'Считаю, что $ должен стоить 35 рублей!'
+    },
+    {
+        subject: 'Гость',
+        text: 'Бесплатно. Скачать. Лучший сайт - http://localhost:3000'
+    }
+];
+
 let AddPost = React.createClass ({
    componentDidMount: function () {
        ReactDOM.findDOMNode(this.refs.subject).focus();
    },
    onBtnClickHandler: function (e) {
        e.preventDefault();
-       let author = ReactDOM.findDOMNode(this.refs.subject).value;
+       let subject = ReactDOM.findDOMNode(this.refs.subject).value;
        let text = ReactDOM.findDOMNode(this.refs.text).value;
-       alert(author+", Ваш блог обновлен!")
+       alert(subject+", Ваш блог обновлен!")
    },
    onCheckRuleClick: function(e) {
        ReactDOM.findDOMNode(this.refs.alert_button).disabled = !e.target.checked;
@@ -56,6 +71,25 @@ let PageHead = React.createClass ({
     }
 });
 
+let PostViewer = React.createClass({
+    render: function () {
+        let data = this.props.data;
+        let blogMessage = data.map(function (item, index) {
+            return (
+                <div key={index}>
+                    <p className="message-author">{item.subject}</p>
+                    <p className="message-text">{item.text}</p>
+                </div>
+            )
+        })
+        return (
+            <div className="message">
+                {blogMessage}
+            </div>
+        )
+    }
+});
+
 let App = React.createClass({
     render: function() {
         return (
@@ -66,7 +100,7 @@ let App = React.createClass({
                     <AddPost/>
                 </div>
                 <div className="post-body">
-
+                    <PostViewer data={blogPost}/>
                 </div>
             </div>
         )
